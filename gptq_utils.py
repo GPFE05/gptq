f977f03c68ea16abd00d42b0d1317646ca23424d
GP <labgp05@gmail.com>
2026-03-17 10:44:09 +0800

    c4

--- gptq_utils.py
+++ gptq_utils.py
@@ -638,21 +638,21 @@ def rtn_fwrd(model, dev, args, logger):
     return quantizers
 
 
-import datasets
 import random
-import transformers
+from datasets import load_dataset
+from transformers import AutoTokenizer
 
 
-def get_c4(nsamples, seed, seqlen, model, trust_remote_code=False):
+def get_c4(nsamples, seed, seqlen, model):
     print("get_c4")
-    traindata = datasets.load_dataset(
+    traindata = load_dataset(
         'allenai/c4', data_files={'train': 'en/c4-train.00000-of-01024.json.gz'}, split='train'
     )
-    valdata = datasets.load_dataset(
+    valdata = load_dataset(
         'allenai/c4', data_files={'validation': 'en/c4-validation.00000-of-00008.json.gz'}, split='validation'
     )
 
-    tokenizer = transformers.AutoTokenizer.from_pretrained(model, use_fast=False)
+    tokenizer = AutoTokenizer.from_pretrained(model, use_fast=False)
 
     random.seed(seed)
     trainloader = []
@@ -684,42 +684,26 @@ def get_c4(nsamples, seed, seqlen, model, trust_remote_code=False):
 
     return trainloader, valenc
 
-def get_wikitext2(nsamples, seed, seqlen, model, hf_token, eval_mode=False):
-    
-    if hf_token is None:
-        tokenizer = transformers.AutoTokenizer.from_pretrained(model, use_fast=False)
-    else:
-        tokenizer = transformers.AutoTokenizer.from_pretrained(model, use_fast=False, use_auth_token=hf_token)
-        
-    if eval_mode:
-        testdata = datasets.load_dataset('Salesforce/wikitext', 'wikitext-2-raw-v1', split='test')
-        testenc = tokenizer("\n\n".join(testdata['text']), return_tensors='pt')
-        return testenc
-    else:
-        traindata = datasets.load_dataset('Salesforce/wikitext', 'wikitext-2-raw-v1', split='train')
-        trainenc = tokenizer("\n\n".join(traindata['text']), return_tensors='pt')    
-        random.seed(seed)
-        trainloader = []
-        for _ in range(nsamples):
-            i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
-            j = i + seqlen
-            inp = trainenc.input_ids[:, i:j]
-            tar = inp.clone()
-            tar[:, :-1] = -100
-            trainloader.append((inp, tar))
-        return trainloader
 
-def get_c4_new(nsamples, seed, seqlen, model, hf_token=None, eval_mode=False):
-    trainloader, valenc = get_c4(nsamples, seed, seqlen, model)
+def get_wikitext2(nsamples, seed, seqlen, model):
+    print("get_wikitext2")
+    traindata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='train')
+    testdata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test')
 
-    if eval_mode:
-        class TokenizerWrapper:
-            def __init__(self, input_ids):
-                self.input_ids = input_ids
+    tokenizer = AutoTokenizer.from_pretrained(model, use_fast=False)
+    trainenc = tokenizer("\n\n".join(traindata['text']), return_tensors='pt')
+    testenc = tokenizer("\n\n".join(testdata['text']), return_tensors='pt')
 
-        return TokenizerWrapper(valenc)
-
-    return trainloader
+    random.seed(seed)
+    trainloader = []
+    for _ in range(nsamples):
+        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
+        j = i + seqlen
+        inp = trainenc.input_ids[:, i:j]
+        tar = inp.clone()
+        tar[:, :-1] = -100
+        trainloader.append((inp, tar))
+    return trainloader, testenc
 
     
 
@@ -728,16 +712,16 @@ def get_ptb_new(nsamples, seed, seqlen, model, hf_token, eval_mode=False):
     
         
     if hf_token is None:
-        tokenizer = transformers.AutoTokenizer.from_pretrained(model, use_fast=False)
+        tokenizer = AutoTokenizer.from_pretrained(model, use_fast=False)
     else:
-        tokenizer = transformers.AutoTokenizer.from_pretrained(model, use_fast=False, use_auth_token=hf_token)
+        tokenizer = AutoTokenizer.from_pretrained(model, use_fast=False, use_auth_token=hf_token)
     
     if eval_mode:
-        testdata = datasets.load_dataset('ptb_text_only', 'penn_treebank', split='test')
+        testdata = load_dataset('ptb_text_only', 'penn_treebank', split='test')
         testenc = tokenizer(" ".join(testdata['sentence']), return_tensors='pt')
         return testenc
     else:
-        traindata = datasets.load_dataset('ptb_text_only', 'penn_treebank', split='train')
+        traindata = load_dataset('ptb_text_only', 'penn_treebank', split='train')
         trainenc = tokenizer(" ".join(traindata['sentence']), return_tensors='pt')
         random.seed(seed)
         trainloader = []
@@ -759,7 +743,7 @@ def get_hard_data(nsamples, seed, seqlen, model,data_path,data_name,from_start=T
         traindata = list(f)
 
 
-    tokenizer = transformers.AutoTokenizer.from_pretrained(model, use_fast=False)
+    tokenizer = AutoTokenizer.from_pretrained(model, use_fast=False)
 
     random.seed(seed)
     trainloader = []
@@ -787,11 +771,21 @@ def get_loaders(
     name, nsamples=128, seed=0, seqlen=2048, model='', hf_token=None, eval_mode=False
 ):
     if 'wikitext2' in name:
-        return get_wikitext2(nsamples, seed, seqlen, model, hf_token, eval_mode)
+        trainloader, testenc = get_wikitext2(nsamples, seed, seqlen, model)
+        if eval_mode:
+            return None, testenc
+        return trainloader
     if 'ptb' in name:
         return get_ptb_new(nsamples, seed, seqlen, model, hf_token, eval_mode)
     if 'c4' in name:
-        return get_c4_new(nsamples, seed, seqlen, model, hf_token, eval_mode)
+        trainloader, valenc = get_c4(nsamples, seed, seqlen, model)
+        if eval_mode:
+            class TokenizerWrapper:
+                def __init__(self, input_ids):
+                    self.input_ids = input_ids
+
+            return None, TokenizerWrapper(valenc)
+        return trainloader
     if 'gsm8k' in name:
         # return get_hard_data(nsamples, seed, seqlen, model,data_path="/home/wsg/llm/bitnet-sliding/gsm8k/gsm8k_train_output_qwen3-8B.jsonl",data_name=name)
         return get_hard_data(nsamples, seed, seqlen, model,data_path="/home/wsg/llm/bitnet-sliding/gsm8k/gsm8k_train_output_qwen3-8B-fewshot-0-V2.jsonl",data_name=name)
